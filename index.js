37dd8d1b28af252ce97314e5598353602ccf595a
Roberto Ramirez <robert.624@hotmail.com>
2023-07-17 15:58:41 -0500

    mobile slider working and rest of slides added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Mobile navigation menu
+
 const mobileHamburger = document.querySelector('.mobile-hamburger');
 
 const mobileNavClosed = document.querySelector('.mobile-nav-closed');
@@ -33,13 +35,65 @@ const nextBtn = document.querySelector('.next');
 const slides = document.querySelectorAll('.slide');
 
 let currentSlide = 0;
-
-const slideWidth = slides[currentSlide].clientWidth;
+let maxSlide = slides.length - 1;
 
 prevBtn.addEventListener('click', () => {
-    console.log('prev');
+    if(currentSlide === 0) {
+        currentSlide = maxSlide;
+    } else {
+        currentSlide--;
+    }
+
+    // Will go to last slide when it reaches the beginning
+    slides.forEach((slide, index) => {
+       slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+    });
 });
 
 nextBtn.addEventListener('click', () => {
-    console.log('next');
+    if(currentSlide === maxSlide) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+
+    // Will go to first slide when it reaches the end
+    slides.forEach((slide, index) => {
+       slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+    });
+});
+
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+// Moving slider with arrow keys
+
+document.addEventListener('keydown', (e) => {
+
+    if(e.key === 'ArrowRight') {
+        if(currentSlide === maxSlide) {
+            currentSlide = 0;
+        } else {
+            currentSlide++;
+        }
+
+        // Will go to first slide when it reaches the end
+        slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+        });
+    }
+
+    if(e.key === 'ArrowLeft') {
+        if(currentSlide === 0) {
+            currentSlide = maxSlide;
+        } else {
+            currentSlide--;
+        }
+
+        // Will go to last slide when it reaches the beginning
+        slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+        });
+    }
 });
